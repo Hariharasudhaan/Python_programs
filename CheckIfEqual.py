b0351fea77ad44f4380f0e8b511fb2d53adfc2f8
Hariharasudhaan <51309475+Hariharasudhaan@users.noreply.github.com>
2019-06-11 13:58:53 +0530

    Program to find two numbers are Equal or Not
    Check Whether the given two numbers are Equal or Not without using Comparison Operator or Arithmetic Operator

---
+++ CheckIfEqual.py
@@ -0,0 +1,6 @@
+x=int(input("Enter the value of x:"))           #Getting X Value
+y=int(input("Enter the value of y:"))           #Getting Y Value
+if ((x ^ y) != 0):                              #Using XOR Operator Check Equal or Not
+    print("Not Equal")
+else:
+    print("Equal") 
