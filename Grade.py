4173e1f977ce243704af121f3d43f679cdd26fe9
Hariharasudhaan <51309475+Hariharasudhaan@users.noreply.github.com>
2019-07-18 09:53:07 +0530

    Program to Find Grade

---
+++ Grade.py
@@ -0,0 +1,15 @@
+n=int(input("Enter the mark:"))
+if(n>=0 and n<40):
+      print("u")
+elif(n>=41 and n<50):
+      print("D")
+elif(n>=51 and n<60):
+      print("C")
+elif(n>=61 and n<70):
+      print("B+")
+elif(n>=71 and n<80):
+      print("B")
+elif(n>=81 and n<90):
+      print("A")
+elif(n>=91 and n<=100):
+      print("S")
